658e6b8b8c719148d1079709cc93f2c98796e9fe
Jaswant15Singh <128139171+Jaswant15Singh@users.noreply.github.com>
2023-03-17 15:55:08 +0530

    Update P1.java

--- LB Assignment-2/P1.java
+++ LB Assignment-2/P1.java
@@ -2,7 +2,7 @@ class P1{
 public static void main(String args[]){
 	int n = 9;
 	for(int i =1;i<=n;i++){
-		for(int j =n;j>i;j--){
+		//for(int j =n;j>i;j--){
 		System.out.print(" ");
 		}for(int k=1;k<=i;k++){
 			System.out.print(i+" ");
