9007f3333a1f117945ab59ed3259968477081e93
RakeshShetty1 <112324754+RakeshShetty1@users.noreply.github.com>
2023-03-19 13:52:10 +0530

    Update ArrayAddSubMul.java

--- Array/ArrayAddSubMul.java
+++ Array/ArrayAddSubMul.java
@@ -53,9 +53,11 @@ class ArrayAddSubMul{
 		            };
 					break;
 			case 3:	System.out.println("Multiplication of Matrix"+"\n"+"Matrix C:");
-	               for(int i=0;i<3;i++){
+	              for(int i=0;i<3;i++){
 			         for(int j=0;j<3;j++){
-				      c[i][j]=a[i][j]*b[i][j];
+						 c[i][j]=0;
+						for(int k=0;k<3;k++){
+						c[i][j]+=a[i][k]*b[k][j];}
 				      System.out.print(c[i][j]+" ");
 		         	}System.out.println();
 		            };
